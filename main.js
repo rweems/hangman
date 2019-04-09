62fe05a41bffadcc4cfb189a3fb8eb94f7d53eb5
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-04-09 16:02:09 -0400

    get random word from selected topic

--- main.js
+++ main.js
@@ -2,13 +2,6 @@
 
 
 // create arrays for topics
-let currentMovies = ['us','shazam','dumbo','captain marvel','pet sematary',
-                'the public','wonder park'];
-
-let topAnime = ['the promised neverland', 'one punch man', 'dororo','mob psycho',
-                'demon slayer', 'fullmetal alchemist','steins gate' ];
-
-
 let htmlWords = ['head','title','body','doctype','tag','href','src'];
 
 let cssWords = ['selector','property','id','class','color','flexbox','border'];
@@ -18,8 +11,17 @@ let jsWords = ['innerhtml','getelementsbyid','document','let','function','object
 
 let jqueryWords = ['click','fadein','fadeout','slidedown','callback','chaining','addclass'];
 
+let currentMovies = ['us','shazam','dumbo','captain marvel','pet sematary',
+                'the public','wonder park'];
+
+ topAnime = ['the promised neverland', 'one punch man', 'dororo','mob psycho',
+                'demon slayer', 'fullmetal alchemist','steins gate' ];
+
+
+
 function createGame()
 {
+    
     let chosenTopic = prompt('Please enter a number from 1 - 6 to choose a topic.' 
                         + '\n1. HTML'
                         + '\n2. CSS'
@@ -28,8 +30,41 @@ function createGame()
                         + '\n5. Current Movies'
                         + '\n6. Top Anime');
 
+    switch(chosenTopic)
+    {
+        case '1':
+            randomWord(htmlWords);
+            break;
+        case '2':
+            randomWord(cssWords);
+            break;
+        case '3':
+            randomWord(jsWords);
+            break;
+        case '4': 
+            randomWord(jqueryWords);
+            break;
+        case '5':
+            randomWord(currentMovies);
+            break;
+        case '6':
+            randomWord(topAnime);
+            break;
+        default:
+            return 'sorry try again';
+    }
 
-    console.log(chosenTopic);
 }
+
+function randomWord(topicArray)
+{
+    
+
+    //https://css-tricks.com/snippets/javascript/select-random-item-array/
+    let hiddenWord = topicArray[Math.floor(Math.random() * topicArray.length)];
+    console.log(hiddenWord);
+}
+
+
 
 createGame();
