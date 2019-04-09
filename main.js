f9e7b8b46394cb96a797bf026db2c17dfac543e2
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-04-09 15:17:24 -0400

    created word arrays

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+// create arrays for topics
+currentMovies = ['us','shazam','dumbo','captain marvel','pet sematary',
+                'the public','wonder park'];
+
+topAnime = ['the promised neverland', 'one punch man', 'dororo','mob psycho',
+                'demon slayer', 'fullmetal alchemist','steins gate' ];
+
+
+htmlWords = ['head','title','body','doctype','tag','href','src'];
+
+cssWords = ['selector','property','id','class','color','flexbox','border'];
+
+jsWords = ['innerhtml','getelementsbyid','document','let','function','object',
+            'array'];
+
+jqueryWords = ['click','fadein','fadeout','slidedown','callback','chaining','addclass'];
+
+console.log(currentMovies);
+console.log(topAnime);
+console.log(htmlWords);
+console.log(cssWords);
+console.log(jsWords);
+console.log(jqueryWords);
